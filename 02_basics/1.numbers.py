74d0437043d890866509a03f1c8aacec69258daf
Pratikkantariya-02 <k4064011@gmail.com>
2026-07-30 13:55:12 +0530

    Add files via upload

---
+++ 02_basics/1.numbers.py
@@ -0,0 +1,21 @@
+import math
+math.floor(2.5)
+math.floor(-2.5)
+math.ceil(2.5)
+math.trunc(2.5)
+math.trunc(-2.5)
+
+
+
+# --------------------------------
+
+import random
+random.randint(1,10)
+
+l1=[1,2,3,4,5]
+random.choice(l1)
+
+random.shuffle(l1)
+
+from decimal import Decimal
+Decimal('0.1') + Decimal('0.1') + Decimal('0.1')
